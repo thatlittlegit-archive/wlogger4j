5ce00a07d5f94d16c4466de8833878d76d3079ed
thatlittlegit <personal@thatlittlegit.tk>
2018-07-02 16:00:34 -0400

    Add test for wlogger4j.LoggerConfig.allows(Logger.Level)

--- src/test/java/wlogger4j/LoggerConfigTest.java
+++ src/test/java/wlogger4j/LoggerConfigTest.java
@@ -1,11 +1,12 @@
 package wlogger4j;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 public class LoggerConfigTest {
-	@Test
-	public void toStringTest() {
+	private LoggerConfig templateConfig() {
 		LoggerConfig lc = new LoggerConfig();
 		lc.trace = true;
 		lc.debug = false;
@@ -15,6 +16,23 @@ public class LoggerConfigTest {
 		lc.warn  = false;
 		lc.error = true;
 		
+		return lc;
+	}
+
+	@Test
+	public void allowsTest( ) {
+		LoggerConfig lc = templateConfig();
+		assertTrue (lc.allows(Logger.Level.TRACE));
+		assertFalse(lc.allows(Logger.Level.DEBUG));
+		assertTrue (lc.allows(Logger.Level.FINER));
+		assertFalse(lc.allows(Logger.Level.FINE ));
+		assertTrue (lc.allows(Logger.Level.INFO ));
+		assertFalse(lc.allows(Logger.Level.WARN ));
+		assertTrue (lc.allows(Logger.Level.ERROR));
+	}
+	@Test
+	public void toStringTest() {
+		LoggerConfig lc = templateConfig();
 		assertEquals(lc.toString(), "LoggerConfig<" + Integer.toHexString(lc.hashCode()) + "> 1010101");
 	}
 }
